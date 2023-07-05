422c25e273ca8c971ed5c64f0634041eab8c0b56
clavedeluna <danalitovsky+git@gmail.com>
2023-07-05 08:43:08 -0300

    handle sensitive commands

--- src/security/safe_command/api.py
+++ src/security/safe_command/api.py
@@ -1,2 +1,50 @@
-def run(original_func, command, *args, **kwargs):
+from pathlib import Path
+import shlex
+from security.exceptions import SecurityException
+
+DEFAULT_CHECKS = frozenset(
+    ("PREVENT_COMMAND_CHAINING", "PREVENT_ARGUMENTS_TARGETING_SENSITIVE_FILES")
+)
+SENSITIVE_FILE_NAMES = frozenset(
+    (
+        "/etc/passwd",
+        "/etc/shadow",
+        "/etc/group",
+        "/etc/gshadow",
+        "/etc/sysconfig/network",
+        "/etc/network/interfaces",
+        "/etc/resolv.conf",
+        "/etc/sudoers",
+        "/etc/hosts",
+    )
+)
+
+
+def run(original_func, command, *args, restrictions=DEFAULT_CHECKS, **kwargs):
+    check(command, restrictions)
     return original_func(command, *args, **kwargs)
+
+
+def check(command, restrictions):
+    assert isinstance(command, (str, list))
+
+    if isinstance(command, str):
+        if not command.strip():
+            # Empty commands are safe
+            return
+        parsed_command = shlex.split(command)
+    if isinstance(command, list):
+        if not command:
+            # Empty commands are safe
+            return
+        parsed_command = command
+
+    if "PREVENT_ARGUMENTS_TARGETING_SENSITIVE_FILES" in restrictions:
+        check_sensitive_files(parsed_command)
+
+
+def check_sensitive_files(parsed_command: list):
+    for cmd in parsed_command:
+        path = Path(cmd)
+        if any(str(path).endswith(sensitive) for sensitive in SENSITIVE_FILE_NAMES):
+            raise SecurityException("Disallowed access to sensitive file: %s", cmd)
--- tests/safe_command/test_api.py
+++ tests/safe_command/test_api.py
@@ -24,3 +24,23 @@ class TestSafeCommandApi:
         expected_result = original_func(command, shell=True)
         safe_api_result = safe_command.run(original_func, command, shell=True)
         self._assert_equal_result(original_func, expected_result, safe_api_result)
+
+    @pytest.mark.parametrize("command", ["", []])
+    def test_empty_command_runs(self, command, original_func):
+        with pytest.raises((PermissionError, IndexError)):
+            safe_command.run(original_func, command)
+
+    @pytest.mark.parametrize(
+        "command",
+        [
+            "cat ///etc//passwd",
+            "cat /etc/passwd",
+            "cat ../../../../../../../../../../../../../../../../../../../../../../../../../../..//etc/passwd",
+            "ls /etc/shadow",
+            "touch /etc/group",
+            "tee /etc/gshadow",
+        ],
+    )
+    def test_blocks_sensitive_files(self, command, original_func):
+        with pytest.raises(SecurityException):
+            safe_command.run(original_func, command)
