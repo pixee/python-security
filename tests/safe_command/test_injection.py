c774f1af73883b572e5158ad98d1bc20b431c138
Lucas Faudman <lucasfaudman@Lucass-MacBook-Pro.local>
2024-03-05 13:40:21 -0800

    Add FuzzDB license.

--- tests/safe_command/test_injection.py
+++ tests/safe_command/test_injection.py
@@ -9,9 +9,9 @@ from security.safe_command.api import _parse_command, _resolve_paths_in_parsed_c
 from security.exceptions import SecurityException
 
 with (Path(__file__).parent / "fuzzdb" / "command-injection-template.txt").open() as f:
-    FUZZDB_OS_COMMAND_INJECTION_PAYLOADS = [line.replace('\\n','\n').replace("\\'", "'")[:-1] for line in f] # Remove newline from the end without modifying payloads and handle escapes
+    FUZZDB_OS_COMMAND_INJECTION_PAYLOADS = [line.replace('\\n','\n').replace("\\'", "'")[:-1] for line in list(f)[1:]] # Remove newline from the end without modifying payloads and handle escapes
 with (Path(__file__).parent / "fuzzdb" / "traversals-8-deep-exotic-encoding.txt").open() as f:
-    FUZZDB_PATH_TRAVERSAL_PAYLOADS = [line.replace('\\n','\n').replace("\\'", "'")[:-1] for line in f]
+    FUZZDB_PATH_TRAVERSAL_PAYLOADS = [line.replace('\\n','\n').replace("\\'", "'")[:-1] for line in list(f)[1:]]
 
 
 @pytest.fixture
